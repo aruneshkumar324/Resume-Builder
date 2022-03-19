5bad0a3e30ea203e41c4ef6a1fd41f9ad1836210
Arunesh kumar <aruneshkumar324@gmail.com>
2022-03-20 05:14:15 +0530

    2.2 : Dashboard - profile and basic setup

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
+# DASHBOARD
 @login_required(login_url='home')
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html')
+
+
+# PROFILE
+@login_required(login_url='home')
+def profile(request):
+    return render(request, 'dashboard/profile.html')
