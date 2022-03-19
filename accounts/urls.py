27fdf69624beca6ee621294db2dafc9947a158da
Arunesh kumar <aruneshkumar324@gmail.com>
2022-03-20 00:10:31 +0530

    1.4 : Accounts - login, register and logout now working

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.login, name="home"),
+    path('', views.user_login, name="home"),
     path('register/', views.register, name='register'),
-    path('test/', views.test, name='test'),
+    path('test_page/', views.test_page, name='test_page'),
+    path('logout/', views.log_out, name='log_out'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
 
 
-def login(request):
-    return render(request, 'accounts/login.html')
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('test_page')
+        else:
+            return render(request, 'accounts/login.html', {"message": "Username not exists"})
+
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+
+            if User.objects.filter(username=username).exists():
+                return render(request, 'accounts/register.html', {"message": "Username not abvalible"})
+            else:
+                if User.objects.filter(email=email).exists():
+                    return render(request, 'accounts/register.html', {"message": "Email not abvalible"})
+                else:
+
+                    # CREATE USER
+                    user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+
+                    login(request, user)
+
+                    user.save()
+
+                    # return render(request, 'accounts/test.html', {"message": "welcome"})
+                    return redirect('test_page')
+
+        else:
+            return render(request, 'accounts/register.html', {"message": "Password not match with confirm password"})
+    else:
+        return render(request, 'accounts/register.html')
 
 
-def test(request):
+def test_page(request):
     return render(request, 'accounts/test.html')
+
+
+def log_out(request):
+    logout(request)
+    return redirect('home')
