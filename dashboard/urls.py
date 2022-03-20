a491512ff022ea240148612ce8f67d0b1790933a
Arunesh kumar <aruneshkumar324@gmail.com>
2022-03-20 20:05:41 +0530

    2.4 : Account settings complete ( update name, email, username, password )

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('profile/', views.profile, name='profile'),
     path('account_settings/', views.account_settings, name='account_settings'),
+    path('new_password/', views.new_password, name='new_password'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
 
 
 # DASHBOARD
@@ -17,4 +19,49 @@ def profile(request):
 # ACCOUNT SETTINGS
 @login_required(login_url='home')
 def account_settings(request):
+    if request.method == 'POST':
+        user_id = request.POST['user_id']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+
+        user = User.objects.get(id=user_id)
+
+        user.username = username
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+
+        user.save()
+
+        return redirect('profile')
+
     return render(request, 'dashboard/account_settings.html')
+
+
+
+@login_required(login_url='home')
+def new_password(request):
+    if request.method == 'POST':
+        user_id = request.POST['user_id']
+        passsword = request.POST['passsword']
+        confirm_passsword = request.POST['confirm_passsword']
+
+        if passsword == confirm_passsword:
+            # set new password
+            data = User.objects.get(id=user_id)
+            data.set_password(passsword)
+
+            data.save()
+
+            # logout
+            logout(request)
+
+            # rediect
+            return redirect('home')
+
+        else:
+            return render(request, 'dashboard/account_settings.html', {"message": "Password not match with confirm password"})
+    else:
+        return render(request, 'dashboard/account_settings.html')
